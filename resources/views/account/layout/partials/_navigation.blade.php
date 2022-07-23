069bf22860cca99d02e9d43480d8c6a84db5e92a
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2022-07-23 20:14:52 +0600

    add helpers

--- resources/views/account/layout/partials/_navigation.blade.php
+++ resources/views/account/layout/partials/_navigation.blade.php
@@ -1,13 +1,13 @@
  <ul class="mt-0">
      <li
-         class="{{ request()->is('account') ? 'bg-green-500 text-white' : '' }} flex w-full justify-between text-black hover:text-white hover:bg-indigo-800 cursor-pointer items-center">
+         class="{{ return_if(on_page('account'), 'bg-green-500 text-white') }} flex w-full justify-between  hover:text-white hover:bg-indigo-800 cursor-pointer items-center">
          <a class="w-full  px-8 py-3" href="{{ route('account.index') }}"
              class="flex items-center  rounded focus:outline-none focus:ring-2 focus:ring-white">
              <span class="text-sm ml-2">Account Overview</span>
          </a>
      </li>
      <li
-         class="{{ request()->is('account/profile') ? 'bg-green-500 text-white' : '' }} flex w-full justify-between text-black hover:text-white hover:bg-indigo-800 cursor-pointer items-center">
+         class="{{ return_if(on_page('account/profile'), 'bg-green-500 text-white') }} flex w-full justify-between  hover:text-white hover:bg-indigo-800 cursor-pointer items-center">
          <a class="w-full  px-8 py-3" href="{{ route('account.profile.index') }}"
              class="flex items-center  rounded focus:outline-none focus:ring-2 focus:ring-white">
              <span class="text-sm ml-2">Profile</span>
@@ -15,7 +15,7 @@
      </li>
 
      <li
-         class="flex w-full justify-between text-black hover:text-white hover:bg-indigo-800 cursor-pointer items-center">
+         class="{{ return_if(on_page('account/password'), 'bg-green-500 text-white') }} flex w-full justify-between  hover:text-white hover:bg-indigo-800 cursor-pointer items-center">
          <a class="w-full  px-8 py-3" href="{{ route('account.password.index') }}"
              class="flex items-center  rounded focus:outline-none focus:ring-2 focus:ring-white">
              <span class="text-sm ml-2">Password</span>
